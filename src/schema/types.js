67487c37df7644b2915de83204637ec001fad205
Sam-2019 <samtmartey@gmail.com>
2021-08-31 21:56:48 +0100

    added typeDefs

---
+++ src/schema/types.js
@@ -0,0 +1,205 @@
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+  enum PledgeStatus {
+    PENDING
+    REDEEMED
+  }
+
+  type SuccessType {
+    message: String
+  }
+
+  type ErrorType {
+    message: String
+  }
+
+  type Payload {
+    error: ErrorType
+  }
+
+  type Child {
+    id: ID
+    firstName: String
+    lastName: String
+  }
+
+  type Department {
+    id: ID
+    name: String
+  }
+
+  type Member {
+    id: ID
+    firstName: String
+    lastName: String
+    otherName: String
+    dateOfBirth: String
+    age: String
+    gender: String
+    hometown: String
+    region: String
+    country: String
+    residentialAddress: String
+    contact: String
+    emergencyContact: String
+    emailAddress: String
+    postalAddress: String
+    maritalStatus: String
+    spouseName: String
+    numberOfChlidren: String
+    nameOfChildren: [Child]
+    dateJoinedChurch: String
+    department: [Department]
+    previousChurch: String
+  }
+
+  input AddMember {
+    id: ID
+    firstName: String
+    lastName: String
+    otherName: String
+    dateOfBirth: String
+    age: String
+    gender: String
+    hometown: String
+    region: String
+    country: String
+    residentialAddress: String
+    contact: String
+    emergencyContact: String
+    emailAddress: String
+    postalAddress: String
+    maritalStatus: String
+    spouseName: String
+    numberOfChlidren: String
+    nameOfChildren: [String]
+    dateJoinedChurch: String
+    department: [String]
+    previousChurch: String
+  }
+
+  type SundaySession {
+    id: ID
+    adultFemale: Int
+    adultMale: Int
+    altercallFemale: Int
+    altercallMen: Int
+    bibleText: String
+    bicycles: Int
+    cars: Int
+    childrenBoy: Int
+    childrenGirl: Int
+    date: String
+    motors: Int
+    omegaFemale: Int
+    omegaMale: Int
+    preacher: String
+    theme: String
+    startTime: String
+    endTime: String
+    visitorsFemale: Int
+    visitorsMale: Int
+  }
+
+  input AddSundaySession {
+    id: ID
+    adultFemale: Int
+    adultMale: Int
+    altercallFemale: Int
+    altercallMen: Int
+    bibleText: String
+    bicycles: Int
+    cars: Int
+    childrenBoy: Int
+    childrenGirl: Int
+    date: String
+    motors: Int
+    omegaFemale: Int
+    omegaMale: Int
+    preacher: String
+    theme: String
+    startTime: String
+    endTime: String
+    visitorsFemale: Int
+    visitorsMale: Int
+  }
+
+  type Tithe {
+    members: [ID]
+  }
+
+  input AddTithePayer {
+    id: ID
+    member: ID
+  }
+
+  type Visitor {
+    id: ID
+    firstName: String
+    lastName: String
+    ageGroup: String
+    awarenessChannel: String
+    awarenessChannelOther: String
+    contact: String
+    date: String
+    invitedBy: String
+    knowingChrist: String
+    location: String
+    membership: String
+    monthOfBirth: String
+  }
+
+  input AddVisitor {
+    id: ID
+    firstName: String
+    lastName: String
+    ageGroup: String
+    awarenessChannel: String
+    awarenessChannelOther: String
+    contact: String
+    date: String
+    invitedBy: String
+    knowingChrist: String
+    location: String
+    membership: String
+    monthOfBirth: String
+  }
+
+  type Pledge {
+    id: ID
+    pledgeeID: ID
+    amount: String
+    programme: String
+    status: PledgeStatus
+    date: String
+  }
+
+  input AddPledge {
+    id: ID
+    pledgeID: ID
+    amount: String
+    programme: String
+    status: PledgeStatus
+    date: String
+  }
+
+  type Query {
+    members: [Member]
+    member(id: ID): Member
+    pledge: [Pledge]
+    visitors: [Visitor]
+    visitor(id: ID): Visitor
+    tithe: [Tithe]
+  }
+
+  type Mutation {
+    addMember(input: AddMember): Payload
+    addSundaySession(input: AddSundaySession): Payload
+    AddTithePayer(input: AddTithePayer): Payload
+    addVisitor(input: AddVisitor): Payload
+    addPledge(input: AddPledge): Payload
+  }
+`;
+
+module.exports = typeDefs;
