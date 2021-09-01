7afb5033f2928fedc553a0cf7310b8217fb4afd7
Sam-2019 <samtmartey@gmail.com>
2021-09-01 11:36:17 +0100

    added resolvers/Query

--- src/resolvers/Query/index.js
+++ src/resolvers/Query/index.js
@@ -1,8 +1,20 @@
 module.exports = {
   members: async (parent, args, { models }) => {
-    return await models.Note.find();
+    return await models.Member.find();
   },
-  member: async (parent, args, { models }) => {
-    return await models.Note.findById(args.id);
+  member: async (parent, { id }, { models }) => {
+    return await models.Member.findById(id);
+  },
+  pledge: async (parent, args, { models }) => {
+    return await models.Pledge.find();
+  },
+  visitors: async (parent, args, { models }) => {
+    return await models.Visitors.find();
+  },
+  visitor: async (parent, { id }, { models }) => {
+    return await models.Visitor.findById(id);
+  },
+  tithe: async (parent, args, { models }) => {
+    return await models.Tithe.find();
   },
 };
