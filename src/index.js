6a849cae46791065524e3f4f15e96c638b5a0f5b
Sam-2019 <samtmartey@gmail.com>
2021-09-03 23:43:47 +0100

    added graphql-depth-limit & graphql-validation-complexity

--- src/index.js
+++ src/index.js
@@ -2,20 +2,23 @@ const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const depthLimit = require("graphql-depth-limit");
+const { createComplexityLimitRule } = require("graphql-validation-complexity");
+// const helmet = require("helmet");
 
 dotenv.config();
-
 
 require("./db");
 const models = require("./db/models");
 const schema = require("./schema");
 
-
 const app = express();
 app.use("*", cors());
 
+
 const server = new ApolloServer({
   schema,
+  validationRules: [depthLimit(5), createComplexityLimitRule(1000)],
   context: () => {
     return { models };
   },
