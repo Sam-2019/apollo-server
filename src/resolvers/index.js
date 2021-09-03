93f1e78604f74c3a3b13b9b78fa8db08b82c63bd
Sam-2019 <samtmartey@gmail.com>
2021-09-03 23:44:07 +0100

    added GraphQLDateTime

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,9 +1,11 @@
+const { GraphQLDateTime } = require("graphql-iso-date");
 const Query = require("./Query");
 const Mutation = require("./Mutation");
 
 const resolvers = {
   Query,
   Mutation,
+  DateTime: GraphQLDateTime,
 };
 
 module.exports = resolvers;
