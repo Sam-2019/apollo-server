0861fe38088382ab8ad9842af1c8a4211dfc8765
Sam-2019 <samtmartey@gmail.com>
2021-09-03 23:42:43 +0100

    updated mutation member

--- src/resolvers/Mutation/member.js
+++ src/resolvers/Mutation/member.js
@@ -1,13 +1,94 @@
-module.exports = {
-    addMember: async (parent, { input: { age, firstName } }, { models }) => {
-      try {
-        return await models.Member.create({
-          age,
-          firstName,
-        });
-      } catch (err) {
-        console.log(err);
-      }
+const { extractMonth, extractYear } = require("../../utils/index");
+
+const addMember = async (
+  parent,
+  {
+    input: {
+      firstName,
+      lastName,
+      otherName,
+      dateOfBirth,
+      age,
+      gender,
+      hometown,
+      region,
+      country,
+      residentialAddress,
+      contact,
+      emergencyContact,
+      emailAddress,
+      postalAddress,
+      maritalStatus,
+      spouseName,
+      numberOfChlidren,
+      nameOfChildren,
+      dateJoinedChurch,
+      department,
+      previousChurch,
     },
-  };
-  
+  },
+  { models }
+) => {
+  const chapel = extractMonth(dateOfBirth);
+
+  try {
+    return await models.Member.create({
+      firstName,
+      lastName,
+      otherName,
+      dateOfBirth,
+      chapel,
+      age,
+      gender,
+      hometown,
+      region,
+      country,
+      residentialAddress,
+      contact,
+      emergencyContact,
+      emailAddress,
+      postalAddress,
+      maritalStatus,
+      spouseName,
+      numberOfChlidren,
+      nameOfChildren,
+      dateJoinedChurch,
+      department,
+      previousChurch,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+const deleteMember = async (parent, { id }, { models }) => {
+  try {
+    return await models.Member.findByIdAndDelete(id);
+  } catch (err) {
+    console.log(err);
+  }
+};
+const updateMember = async (
+  parent,
+  { id, input: { firstName, age } },
+  { models }
+) => {
+  try {
+    return await models.Member.findByIdAndUpdate(
+      id,
+      {
+        $set: { firstName, age },
+      },
+      {
+        new: true,
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = {
+  addMember,
+  deleteMember,
+  updateMember,
+};
