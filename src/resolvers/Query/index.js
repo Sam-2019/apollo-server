09770d9d3c5bfa057c3836f929a31d0360e54ac8
Sam-2019 <samtmartey@gmail.com>
2021-09-03 23:42:16 +0100

    updated query index

--- src/resolvers/Query/index.js
+++ src/resolvers/Query/index.js
@@ -1,20 +1,82 @@
+const members = async (parent, args, { models }) => {
+  return await models.Member.find();
+};
+const membersFeed = async (parent, { cursor }, { models }) => {
+  const limit = 10;
+  let hasNextPage = false;
+  let cursorQuery = {};
+
+  if (cursor) {
+    cursorQuery = { _id: { $lt: cursor } };
+  }
+
+  let members = await models.Member.find(cursorQuery)
+    .sort({ _id: -1 })
+    .limit(limit + 1);
+
+  if (members.length > limit) {
+    hasNextPage = true;
+    members = members.slice(0, -1);
+  }
+
+  const newCursor = members[members.length - 1]._id;
+
+  return {
+    members,
+    cursor: newCursor,
+    hasNextPage,
+  };
+};
+const member = async (parent, { id }, { models }) => {
+  return await models.Member.findById(id);
+};
+const pledge = async (parent, args, { models }) => {
+  return await models.Pledge.find().limit(10);
+};
+const pledgeFeed = async (parent, args, { models }) => {
+  return await models.Pledge.find();
+};
+const visitors = async (parent, args, { models }) => {
+  return await models.Visitors.find().limit(10);
+};
+const visitorsFeed = async (parent, args, { models }) => {
+  return await models.Visitors.find();
+};
+const visitor = async (parent, { id }, { models }) => {
+  return await models.Visitor.findById(id);
+};
+const tithe = async (parent, args, { models }) => {
+  return await models.Tithe.find().limit(10);
+};
+const titheFeed = async (parent, args, { models }) => {
+  return await models.Tithe.find();
+};
+const sundayService = async (parent, args, { models }) => {
+  return await models.SundayService.find().limit(10);
+};
+const sundayServiceFeed = async (parent, args, { models }) => {
+  return await models.SundayService.find();
+};
+
+const chapel = async (parent, { chapel }, { models }) => {
+
+  return await models.Member.find({
+    chapel: { $regex: chapel, $options: "i" },
+  });
+};
+
 module.exports = {
-  members: async (parent, args, { models }) => {
-    return await models.Member.find();
-  },
-  member: async (parent, { id }, { models }) => {
-    return await models.Member.findById(id);
-  },
-  pledge: async (parent, args, { models }) => {
-    return await models.Pledge.find();
-  },
-  visitors: async (parent, args, { models }) => {
-    return await models.Visitors.find();
-  },
-  visitor: async (parent, { id }, { models }) => {
-    return await models.Visitor.findById(id);
-  },
-  tithe: async (parent, args, { models }) => {
-    return await models.Tithe.find();
-  },
+  members,
+  membersFeed,
+  member,
+  pledge,
+  pledgeFeed,
+  visitors,
+  visitorsFeed,
+  visitor,
+  tithe,
+  titheFeed,
+  sundayService,
+  sundayServiceFeed,
+  chapel,
 };
