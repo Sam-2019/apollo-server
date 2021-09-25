2951bd6603380ca21b350855756b33c9048d3151
Sam-2019 <samtmartey@gmail.com>
2021-09-26 00:24:14 +0100

    updated switchModel

--- src/utils/switchModel.js
+++ src/utils/switchModel.js
@@ -31,6 +31,44 @@ const paymentType = async (type) => {
   return await model;
 };
 
+const groupType = async (type) => {
+  let model;
+  let limit = 16;
+
+  switch (type) {
+    case "adult":
+      model = models.Adult;
+      
+      break;
+
+    case "omega":
+      model = models.Omega;
+      break;
+
+    case "children":
+      model = models.Children;
+      break;
+
+    case "vehicles":
+      model = models.Vehicle;
+      limit = 12;
+      break;
+
+    case "sundayTotal":
+      model = models.SundayTotal;
+      break;
+
+    default:
+      model = null;
+  }
+
+  return {
+    model,
+    limit
+  };
+};
+
 module.exports = {
   paymentType,
+  groupType,
 };
