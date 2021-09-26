8dda34381ff673b2363ecf719453400527fe2868
Sam-2019 <samtmartey@gmail.com>
2021-09-26 02:34:35 +0100

    code comment

--- src/resolvers/Mutation/sundayService.js
+++ src/resolvers/Mutation/sundayService.js
@@ -1,3 +1,5 @@
+const { sumData } = require("../../utils/index");
+
 const addSundayService = async (
   parent,
   {
@@ -25,13 +27,34 @@ const addSundayService = async (
   },
   { models }
 ) => {
-  const vehicleArray = [
-    { key: "Cars", value: cars },
-    { key: "Motors", value: motors },
-    { key: "Bicycles", value: bicycles },
-  ];
+  // const groupTotal = [
+  //   { key: "Adult", value: sumData(adultMale, adultFemale) },
+  //   { key: "Omega", value: sumData(omegaMale, omegaFemale) },
+  //   { key: "Children", value: sumData(childrenBoy, childrenGirl) },
+  // ];
+
+  // const vehicleArray = [
+  //   { key: "Cars", value: cars },
+  //   { key: "Motors", value: motors },
+  //   { key: "Bicycles", value: bicycles },
+  // ];
+
+  // const adultArray = [
+  //   { key: "Male", value: adultMale },
+  //   { key: "Female", value: adultFemale },
+  // ];
+
+  // const omegaArray = [
+  //   { key: "Male", value: omegaMale },
+  //   { key: "Female", value: omegaFemale },
+  // ];
+
+  // const childrenArray = [
+  //   { key: "Male", value: childrenBoy },
+  //   { key: "Female", value: childrenGirl },
+  // ];
   try {
-    const sundayServiceData = await models.SundayService.create({
+    return await models.SundayService.create({
       adultFemale,
       adultMale,
       altercallFemale,
@@ -53,14 +76,50 @@ const addSundayService = async (
       type,
     });
 
-    vehicleArray.forEach(async (text) => {
-      await models.Vehicle.create({
-        sundayService: sundayServiceData.id,
-        type: text.key,
-        date,
-        number: text.value,
-      });
-    });
+    // vehicleArray.forEach(async (text) => {
+    //   await models.Vehicle.create({
+    //     sundayService: sundayServiceData.id,
+    //     group: text.key,
+    //     date,
+    //     value: text.value,
+    //   });
+    // });
+
+    // adultArray.forEach(async (text) => {
+    //   await models.Adult.create({
+    //     sundayService: sundayServiceData.id,
+    //     group: text.key,
+    //     date,
+    //     value: text.value,
+    //   });
+    // });
+
+    // omegaArray.forEach(async (text) => {
+    //   await models.Omega.create({
+    //     sundayService: sundayServiceData.id,
+    //     group: text.key,
+    //     date,
+    //     value: text.value,
+    //   });
+    // });
+
+    // childrenArray.forEach(async (text) => {
+    //   await models.Children.create({
+    //     sundayService: sundayServiceData.id,
+    //     group: text.key,
+    //     date,
+    //     value: text.value,
+    //   });
+    // });
+
+    // groupTotal.forEach(async (text) => {
+    //   await models.SundayTotal.create({
+    //     sundayService: sundayServiceData.id,
+    //     group: text.key,
+    //     date,
+    //     value: text.value,
+    //   });
+    // });
   } catch (err) {
     console.log(err);
   }
