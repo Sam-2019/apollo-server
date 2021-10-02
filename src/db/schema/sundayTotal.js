6b4f3d3f485009f7cccb2f11d70100750ee89732
Sam-2019 <samtmartey@gmail.com>
2021-10-02 19:37:04 +0100

    added sunday

---
+++ src/db/schema/sundayTotal.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const sundayTotalSchema = new mongoose.Schema({
+  sundayService: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "SundayService",
+  },
+  type: {
+    type: String,
+  },
+  date: {
+    type: String,
+  },
+  value: {
+    type: Number,
+  },
+});
+
+const SundayTotal = mongoose.model("SundayTotal", sundayTotalSchema);
+module.exports = SundayTotal;
