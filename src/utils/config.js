5d829f90bc9d77a428c7f934c570116f2a759a22
Sam-2019 <samtmartey@gmail.com>
2021-10-17 00:19:14 +0100

    added config

---
+++ src/utils/config.js
@@ -0,0 +1,17 @@
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
+const USER_EMAIL = process.env.USER_EMAIL;
+const USER_NAME = process.env.USER_EMAIL;
+const RECEPIENTS_EMAIL = process.env.RECEPIENTS_EMAIL;
+
+module.exports = {
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI,
+  REFRESH_TOKEN,
+  USER_EMAIL,
+  USER_NAME,
+  RECEPIENTS_EMAIL,
+};
