6c16bd41322e02689dcbf78976211c0f9062e6d4
Sam-2019 <samtmartey@gmail.com>
2021-12-11 15:03:40 +0000

    updated redis

--- src/services/redis.js
+++ src/services/redis.js
@@ -1,5 +1,5 @@
 const Redis = require("ioredis");
-const { REDIS_HOST, REDIS_PORT } = require("../utils/config");
+const { REDIS_HOST, REDIS_PORT, REDIS_PASSWORD } = require("../utils/config");
 const { mailer } = require("./nodemailer");
 
 const redisClient = new Redis({
@@ -9,35 +9,50 @@ const redisClient = new Redis({
 });
 
 Redis.Command.setReplyTransformer("hgetall", (data) => {
+  if (!data) return console.error("No data");
+
   const arr = [];
-  for (let i = 0; i < data.length; i += 2) {
-    arr.push({ name: data[i], email: data[i + 1] });
+
+  try {
+    for (let i = 0; i < data.length; i += 2) {
+      arr.push({ name: data[i], email: data[i + 1] });
+    }
+
+    return arr;
+  } catch (error) {
+    console.log(error);
   }
-  return arr;
 });
 
 function writeRedis(key, name, email) {
-  if (!key && !name && !email) return console.error("No data");
+  if (!key || !name || !email) return console.error("No data");
   return redisClient.hset(key, name, email);
 }
 
 const expireRedisItem = (key, data, response) => {
-  if (key === undefined || data === undefined) return console.error("No data");
-  if (key.length === 0 || data.length === 0) return console.error("No data");
+  if (!key || !data || !response) return console.error("No data");
 
   if (data.length === response.length) {
     redisClient.expire(key, 600);
-    return "Expiration successful";
+    return console.log("Expiration successful");
   }
 };
 
-const getEmailsFromRedisAndSend = async (key) => {
+const getEmailsFromRedisAndSend = (key) => {
+  if (!key) return console.log("No key");
+
   redisClient.hgetall(key, async (err, data) => {
     if (err) return err;
-    if (data.length === 0) return null;
+    if (!data) return console.log("empty");
 
-    const response = await mailer(data);
-    return expireRedisItem(key, data, response);
+    try {
+      const response = await mailer(data);
+
+      console.log({ response: response });
+      return expireRedisItem(key, data, response);
+    } catch (err) {
+      console.log(err);
+    }
   });
 };
 
