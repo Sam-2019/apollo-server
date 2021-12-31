f6f368f5f28973839a5d5309859675113a0edf55
Sam-2019 <samtmartey@gmail.com>
2021-12-31 20:38:35 +0000

    code update

--- index.js
+++ index.js
@@ -13,46 +13,46 @@ const result = dotenv.config();
 const { redisClient } = require("./src/services/redis");
 const { graceful, bree } = require("./src/services/bree");
 
-if (result.error) {
-  throw result.error;
-}
-
-redisClient;
-graceful.listen();
-bree.start();
-
 require("./src/db");
 
 const models = require("./src/db/models");
 const schema = require("./src/schema");
 
-const app = express();
-app.use("*", cors());
-
-const server = new ApolloServer({
-  schema,
-  validationRules: [depthLimit(5), createComplexityLimitRule(1000)],
-  context: () => {
-    return { models };
-  },
-  plugins: [
-    process.env.NODE_ENV === "production"
-      ? ApolloServerPluginLandingPageDisabled()
-      : ApolloServerPluginLandingPageLocalDefault({ footer: false }),
-  ],
-});
-
 async function data() {
+  if (result.error) {
+    throw result.error;
+  }
+
+  redisClient;
+  graceful.listen();
+  bree.start();
+
+  const app = express();
+  app.use("*", cors());
+
+  const server = new ApolloServer({
+    schema,
+    validationRules: [depthLimit(5), createComplexityLimitRule(1000)],
+    context: () => {
+      return { models };
+    },
+    plugins: [
+      process.env.NODE_ENV === "production"
+        ? ApolloServerPluginLandingPageDisabled()
+        : ApolloServerPluginLandingPageLocalDefault({ footer: false }),
+    ],
+  });
+
   await server.start();
   server.applyMiddleware({ app, path: "/graphql" });
+
+  const port = process.env.PORT || 4000;
+
+  app.listen({ port }, () => {
+    console.log(
+      `Apollo Server on  http://localhost:${port}${server.graphqlPath}`
+    );
+  });
 }
 
-data();
-
-const port = process.env.PORT || 4000;
-
-app.listen({ port }, () => {
-  console.log(
-    `Apollo Server on  http://localhost:${port}${server.graphqlPath}`
-  );
-});
+data().catch((err) => console.error(err));
