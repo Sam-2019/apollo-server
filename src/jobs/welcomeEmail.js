09f92224901fd7ab173260f0494b713e7c45cf1b
Sam-2019 <samtmartey@gmail.com>
2022-01-04 22:21:11 +0000

    added visitor mailer

--- src/jobs/welcomeEmail.js
+++ src/jobs/welcomeEmail.js
@@ -1,4 +1,7 @@
 const { getEmailsFromRedisAndSend } = require("../services/redis");
 
 const memberKey = "h3";
+const visitorKey = "h4";
+
 getEmailsFromRedisAndSend(memberKey);
+getEmailsFromRedisAndSend(visitorKey);
