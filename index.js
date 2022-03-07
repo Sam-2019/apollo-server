0513b4d670ab3aa522779b29617925c3f6c4fcc7
Sam-2019 <samtmartey@gmail.com>
2022-03-07 09:39:09 +0000

    index update

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
-const {
-    ApolloServerPluginLandingPageLocalDefault,
-    ApolloServerPluginLandingPageDisabled,
-  } = require("apollo-server-core");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const depthLimit = require("graphql-depth-limit");
@@ -25,11 +21,6 @@ const server = new ApolloServer({
   context: () => {
     return { models };
   },
-  plugins: [
-    process.env.NODE_ENV === "production"
-      ? ApolloServerPluginLandingPageDisabled()
-      : ApolloServerPluginLandingPageLocalDefault({ footer: false }),
-  ],
 });
 
 async function data() {
