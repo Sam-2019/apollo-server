47ef53257895f06523588086231cbf58e1ca82a1
Sam-2019 <samtmartey@gmail.com>
2022-03-07 09:10:06 +0000

    rm redis

--- src/services/redis.js
+++ src/services/redis.js
@@ -1,76 +1,76 @@
-const Redis = require("ioredis");
-const {
-  REDIS_HOST,
-  REDIS_PORT,
-  REDIS_PASSWORD,
-  REDIS_USERNAME,
-  NODE_ENV,
-  REDIS_HOST_LOCAL,
-  REDIS_PORT_LOCAL,
-  REDIS_PASSWORD_LOCAL,
-} = require("../utils/config");
-const { mailer } = require("./nodemailer");
+// const Redis = require("ioredis");
+// const {
+//   REDIS_HOST,
+//   REDIS_PORT,
+//   REDIS_PASSWORD,
+//   REDIS_USERNAME,
+//   NODE_ENV,
+//   REDIS_HOST_LOCAL,
+//   REDIS_PORT_LOCAL,
+//   REDIS_PASSWORD_LOCAL,
+// } = require("../utils/config");
+// const { mailer } = require("./nodemailer");
 
-const redisClient = new Redis({
-  host: NODE_ENV === "production" ? REDIS_HOST : REDIS_HOST_LOCAL,
-  port: NODE_ENV === "production" ? REDIS_PORT : REDIS_PORT_LOCAL,
-  password: NODE_ENV === "production" ? REDIS_PASSWORD : null,
-  username: NODE_ENV === "production" ? REDIS_USERNAME : null,
-});
+// const redisClient = new Redis({
+//   host: NODE_ENV === "production" ? REDIS_HOST : REDIS_HOST_LOCAL,
+//   port: NODE_ENV === "production" ? REDIS_PORT : REDIS_PORT_LOCAL,
+//   password: NODE_ENV === "production" ? REDIS_PASSWORD : null,
+//   username: NODE_ENV === "production" ? REDIS_USERNAME : null,
+// });
 
-Redis.Command.setReplyTransformer("hgetall", async (data) => {
-  if (data.length === 0) return console.error({ transformer: "No data" });
+// Redis.Command.setReplyTransformer("hgetall", async (data) => {
+//   if (data.length === 0) return console.error({ transformer: "No data" });
 
-  const arr = [];
+//   const arr = [];
 
-  try {
-    for (let i = 0; i < data.length; i += 2) {
-      arr.push({ name: data[i], email: data[i + 1] });
-    }
+//   try {
+//     for (let i = 0; i < data.length; i += 2) {
+//       arr.push({ name: data[i], email: data[i + 1] });
+//     }
 
-    return arr;
-  } catch (error) {
-    console.log(error);
-  }
-});
+//     return arr;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
-function writeRedis(key, name, email) {
-  if (!key || !name || !email) return console.error({ writeRedis: "No data" });
-  return redisClient.hset(key, name, email);
-}
+// function writeRedis(key, name, email) {
+//   if (!key || !name || !email) return console.error({ writeRedis: "No data" });
+//   return redisClient.hset(key, name, email);
+// }
 
-const expireRedisItem = (key, data, response) => {
-  if (response.length === 0) return console.error({ expireRedis: "No data" });
+// const expireRedisItem = (key, data, response) => {
+//   if (response.length === 0) return console.error({ expireRedis: "No data" });
 
-  if (!key || !data || !response)
-    return console.error({ expireRedis: "No data" });
+//   if (!key || !data || !response)
+//     return console.error({ expireRedis: "No data" });
 
-  if (data.length === response.length) {
-    redisClient.expire(key, 600);
-    return console.log("Expiration successful");
-  }
-};
+//   if (data.length === response.length) {
+//     redisClient.expire(key, 600);
+//     return console.log("Expiration successful");
+//   }
+// };
 
-const getEmailsFromRedisAndSend = (key) => {
-  if (!key) return console.log({ getEmails: "No key" });
+// const getEmailsFromRedisAndSend = (key) => {
+//   if (!key) return console.log({ getEmails: "No key" });
 
-  redisClient.hgetall(key, async (err, data) => {
-    if (err) return err;
-    if (!data) return console.error({ getEmails: "No data" });
+//   redisClient.hgetall(key, async (err, data) => {
+//     if (err) return err;
+//     if (!data) return console.error({ getEmails: "No data" });
 
-    try {
-      const response = await mailer(data);
+//     try {
+//       const response = await mailer(data);
 
-      return expireRedisItem(key, data, response);
-    } catch (err) {
-      console.log(err);
-    }
-  });
-};
+//       return expireRedisItem(key, data, response);
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   });
+// };
 
-module.exports = {
-  redisClient,
-  writeRedis,
-  expireRedisItem,
-  getEmailsFromRedisAndSend,
-};
+// module.exports = {
+//   redisClient,
+//   writeRedis,
+//   expireRedisItem,
+//   getEmailsFromRedisAndSend,
+// };
