5ed3688352a941279521f1121a43d98d3ad5c929
Sam-2019 <samtmartey@gmail.com>
2022-03-07 09:26:34 +0000

    code update

--- index.js
+++ index.js
@@ -1,40 +1,62 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
+const {
+  ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageDisabled,
+} = require("apollo-server-core");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const depthLimit = require("graphql-depth-limit");
 const { createComplexityLimitRule } = require("graphql-validation-complexity");
+const result = dotenv.config();
 
-dotenv.config();
+// const { redisClient } = require("./src/services/redis");
+// const { graceful, bree } = require("./src/services/bree");
+// const { bot } = require("./src/services/telegram");
 
 require("./src/db");
+
 const models = require("./src/db/models");
 const schema = require("./src/schema");
 
-const app = express();
-app.use("*", cors());
-
-
-const server = new ApolloServer({
-  schema,
-  validationRules: [depthLimit(5), createComplexityLimitRule(1000)],
-  context: () => {
-    return { models };
-  },
-});
-
 async function data() {
-  await server.start();
+  if (result.error) {
+    throw result.error;
+  }
 
+  // redisClient;
+  // graceful.listen();
+  // bree.start();
+  // bot.launch();
+
+  const app = express();
+  app.use("*", cors());
+
+  const server = new ApolloServer({
+    schema,
+    validationRules: [depthLimit(5), createComplexityLimitRule(1000)],
+    context: () => {
+      return { models };
+    },
+    plugins: [
+      process.env.NODE_ENV === "production"
+        ? ApolloServerPluginLandingPageDisabled()
+        : ApolloServerPluginLandingPageLocalDefault({ footer: false }),
+    ],
+  });
+
+  await server.start();
   server.applyMiddleware({ app, path: "/graphql" });
+
+  const port = process.env.PORT || 4000;
+
+  app.listen({ port }, () => {
+    console.log(
+      process.env.NODE_ENV === "production"
+        ? `server live`
+        : `server live on http://localhost:${port}${server.graphqlPath}`
+    );
+  });
 }
 
-data();
-
-const port = process.env.PORT || 4000;
-
-app.listen({ port }, () => {
-  console.log(
-    `Apollo Server on  http://localhost:${port}${server.graphqlPath}`
-  );
-});
+data().catch((err) => console.error(err));
