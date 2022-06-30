bf6c245cf3dadb8dc66309ab777f1eb0b926fb60
Sam-2019 <samtmartey@gmail.com>
2022-07-01 00:25:07 +0100

    added job

---
+++ src/db/schema/job.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const jobSchema = new mongoose.Schema({
+  title: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  domain: {
+    type: String,
+  },
+  imgURL: {
+    type: String,
+  },
+  favicon: {
+    type: String,
+  },
+  url: {
+    type: String,
+  },
+});
+
+const Job = mongoose.model("Job", jobSchema);
+module.exports = Job;
