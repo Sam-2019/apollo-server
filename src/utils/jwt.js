4cc889e78cd2933e2be1b4b3c1e194881c76bbb2
Sam-2019 <samtmartey@gmail.com>
2022-07-02 20:35:49 +0100

    updated jwt

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,36 +1,59 @@
-const jwt = require("jsonwebtoken");
+const { sign, verify } = require("jsonwebtoken");
 const { TOKEN } = require("./config");
 
 const generateAccessToken = (user) => {
   if (!user) return false;
-  return jwt.sign({ id: user._id }, TOKEN, {
+  return sign({ id: user._id }, TOKEN, {
     expiresIn: "15m",
   });
 };
 
-const validateAccessToken = (token) => {
-  if (!user) return;
-  return jwt.verify(token, TOKEN, {
+const generaterefreshToken = (user) => {
+  if (!user) return false;
+  return sign({ id: user._id }, TOKEN, {
     expiresIn: "7d",
   });
 };
 
-const getUser = (token) => {
-  if (token) {
-    try {
-      // return the user information from the token
-      return jwt.verify(token, TOKEN);
-    } catch (err) {
-      // if there's a problem with the token, throw an error
-      throw new Error("Session Invalid");
-    }
+const validateAccessToken = (token, next) => {
+  if (!token) return next();
+
+  try {
+    return verify(token, TOKEN, {
+      expiresIn: "7d",
+    });
+  } catch {
+    throw new Error("Invalid access token");
+  }
+};
+
+function validateRefreshToken(token, next) {
+  if (!token) return next();
+  try {
+    return verify(token, TOKEN, {
+      expiresIn: "7d",
+    });
+  } catch {
+    throw new Error("Invalid refresh token");
+  }
+}
+
+const getUser = (token, next) => {
+  if (!token) {
+    return next();
   }
 
-  return new Error("Invalid Request");
+  try {
+    // return the user information from the token
+    return validateAccessToken(token, next);
+  } catch (err) {
+    // if there's a problem with the token, throw an error
+    throw new Error("Session Invalid");
+  }
 };
 
 module.exports = {
   generateAccessToken,
-  validateAccessToken,
+  generaterefreshToken,
   getUser,
 };
