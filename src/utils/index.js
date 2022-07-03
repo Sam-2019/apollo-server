708a41f4bca0463fbbdb64a6ad1a4b154583e687
Sam-2019 <samtmartey@gmail.com>
2022-07-03 07:35:19 +0100

    code cleanup

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,18 +1,7 @@
-const linkPreviewGenerator = require("link-preview-generator");
 const countries = require("i18n-iso-countries");
 const PhoneNumber = require("awesome-phonenumber");
 const bcrypt = require("bcryptjs");
-const { Job } = require("../db/models");
 const saltRounds = 12;
-
-const url_pattern =
-  /[A-Za-z]+:\/\/[A-Za-z0-9\-_]+\.[A-Za-z0-9\-_:%&;\?\#\/.=]+/g;
-const description_pattern = /^.*?(?=-)/gm;
-const special_characters = /['“”\\\\]/gm;
-
-String.prototype.rm_space = function () {
-  return this.replace(/((\s*\S+)*)\s*/, "$1");
-};
 
 // const validate = require("deep-email-validator");
 // export const extractMonth = (value) => {
@@ -61,61 +50,10 @@ const transformNumber = (country, contact) => {
   return pn.getNumber();
 };
 
-const checkTwiter = async (data) => {
-  // console.log({ TwitterLink: data });
-
-  const info = await linkPreviewGenerator(data);
-
-  if (!info) {
-    return;
-  }
-
-  const sanitized_description = info.description.replace(
-    special_characters,
-    ""
-  );
-  const description = String(sanitized_description.match(description_pattern));
-
-  const url = String(sanitized_description.match(url_pattern));
-
-  try {
-    const saveData = await Job.create({
-      title: info.title,
-      description: description.rm_space(),
-      domain: info.domain,
-      imgURL: info.img,
-      favicon: info.favicon,
-      url: url,
-    });
-
-    if (!saveData) {
-      return "Error!";
-    }
-
-    return "Data saved!";
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-// const trial = () => {
-//   const source = {
-//     title: "Vacancies in Ghana on Twitter",
-//     description: "“Female Marketing Assistant - https://t.co/eUTRrZVD90”",
-//     domain: "twitter.com",
-//     img: "https://pbs.twimg.com/profile_images/1231236382808563713/Q88JDBjw_400x400.jpg",
-//     favicon: "https://twitter.com/favicon.ico",
-//   };
-
-//   const sanitized_description = source.description.replace(
-//     special_characters,
-//     ""
-//   );
-//   const description = String(sanitized_description.match(description_pattern));
-//   console.log(description);
-// };
-
-// trial();
+const url_pattern =
+  /[A-Za-z]+:\/\/[A-Za-z0-9\-_]+\.[A-Za-z0-9\-_:%&;\?\#\/.=]+/g;
+const description_pattern = /^.*?(?=-)/gm;
+const special_characters = /['“”\\\\]/gm;
 
 module.exports = {
   extractMonth,
@@ -125,5 +63,7 @@ module.exports = {
   comparePassword,
   // isEmail,
   transformNumber,
-  checkTwiter,
+  url_pattern,
+  description_pattern,
+  special_characters,
 };
