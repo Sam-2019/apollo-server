b1bbafb1f5591b420c889dd44ab237d64601f235
Sam-2019 <samtmartey@gmail.com>
2022-07-05 07:49:28 +0100

    added timestamp

--- src/db/schema/job.js
+++ src/db/schema/job.js
@@ -1,26 +1,28 @@
 const mongoose = require("mongoose");
 
-const jobSchema = new mongoose.Schema({
-  title: {
-    type: String,
+const jobSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    domain: {
+      type: String,
+    },
+    imgURL: {
+      type: String,
+    },
+    favicon: {
+      type: String,
+    },
+    url: {
+      type: String,
+    },
   },
-  description: {
-    type: String,
-  },
-  domain: {
-    type: String,
-  },
-  imgURL: {
-    type: String,
-  },
-  favicon: {
-    type: String,
-  },
-  url: {
-    type: String,
-  },
-  timestamps: { createdAt: Date, updatedAt: Date },
-});
+  { timestamps: true }
+);
 
 const Job = mongoose.model("Job", jobSchema);
 module.exports = Job;
