c96c675d8ee94a95f6cf15287be9d78057f29aa4
Sam-2019 <samtmartey@gmail.com>
2022-08-04 22:19:45 +0100

    added jwt

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,41 +1,29 @@
+const { ApolloError } = require("apollo-server-errors");
 const { sign, verify } = require("jsonwebtoken");
-const { TOKEN } = require("./config");
+const { ACCESS_SECRET, REFRESH_SECRET } = require("./config");
 
 const generateAccessToken = (user) => {
   if (!user) return false;
-  return sign({ id: user._id }, TOKEN, {
-    expiresIn: "15m",
+  return sign({ id: String(user._id) }, ACCESS_SECRET, {
+    expiresIn: "15min",
   });
 };
 
-const generaterefreshToken = (user) => {
-  if (!user) return false;
-  return sign({ id: user._id }, TOKEN, {
+const generateRefreshToken = (user) => {
+  if (!user) return "";
+  return sign({ id: String(user._id) }, REFRESH_SECRET, {
     expiresIn: "7d",
   });
 };
 
 const validateAccessToken = (token) => {
   if (!token) return;
-
-  try {
-    return verify(token, TOKEN, {
-      expiresIn: "7d",
-    });
-  } catch {
-    throw new Error("Invalid access token");
-  }
+  return verify(token, ACCESS_SECRET);
 };
 
 const validateRefreshToken = (token) => {
   if (!token) return;
-  try {
-    return verify(token, TOKEN, {
-      expiresIn: "7d",
-    });
-  } catch {
-    throw new Error("Invalid refresh token");
-  }
+  return verify(token, REFRESH_SECRET);
 };
 
 const getUser = (token) => {
@@ -48,12 +36,45 @@ const getUser = (token) => {
     return validateAccessToken(token);
   } catch (err) {
     // if there's a problem with the token, throw an error
-    throw new Error("Session Invalid");
+    throw new ApolloError("Session Invalid");
   }
 };
 
+function setCookie() {
+  const nowDate = Date.now();
+  const date = new Date(nowDate);
+  const time = new Date(nowDate);
+  const extendDate = date.setDate(date.getDate() + 5);
+  const extendTime = time.setMinutes(time.getMinutes() + 20);
+  // console.log({ extendDate: new Date(extendDate).toUTCString() });
+  // console.log({ extendTime: new Date(extendTime).toUTCString() });
+
+  return {
+    access: extendTime,
+    refresh: extendDate,
+  };
+}
+
+const sendRefreshToken = (res, user) => {
+  res.cookie("refreshToken", generateRefreshToken(user), {
+    httpOnly: true,
+    path: "/refresh_token",
+  });
+};
+
+// const sendAccessToken = (res, user) => {
+//   res.cookie("access-token", generateRefreshToken(user), {
+//     httpOnly: true,
+//     path: "/refresh_token",
+//   });
+// };
+
 module.exports = {
   generateAccessToken,
-  generaterefreshToken,
+  generateRefreshToken,
+  validateAccessToken,
+  validateRefreshToken,
   getUser,
+  setCookie,
+  sendRefreshToken,
 };
