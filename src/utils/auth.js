6d0f7190ead5bed75c93172c38f4c09fd8e45883
Sam-2019 <samtmartey@gmail.com>
2022-08-04 22:20:51 +0100

    added auth

---
+++ src/utils/auth.js
@@ -0,0 +1,23 @@
+const { validateAccessToken } = require("./jwt");
+
+const isAuth = (req, res, next) => {
+  const authorization = req.headers["authorization"];
+  console.log({ authorization: authorization });
+
+  if (!authorization) {
+    // throw new AuthenticationError("not authenticated");
+    return next();
+  }
+
+  try {
+    const token = authorization.split(" ")[1];
+    const payload = validateAccessToken(token);
+    req.id = payload.id;
+  } catch {}
+
+  return next();
+};
+
+module.exports = {
+  isAuth,
+};
