3ba59698c80b71491475150b61cb4b513ead406e
Sam-2019 <samtmartey@gmail.com>
2022-08-14 23:16:48 +0100

    process check

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -33,7 +33,8 @@ const getUser = (token) => {
 
   try {
     // return the user information from the token
-    return validateAccessToken(token);
+    const data = validateAccessToken(token);
+    console.log({ data: data });
   } catch (err) {
     // if there's a problem with the token, throw an error
     throw new ApolloError("Session Invalid");
